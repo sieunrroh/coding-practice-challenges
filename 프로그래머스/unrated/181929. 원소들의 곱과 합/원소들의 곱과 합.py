130f9d877df3b1324c2c2e0282fe9e73575ab63f
sieunrroh <151046260+sieunrroh@users.noreply.github.com>
2023-12-19 23:23:31 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,6 +1,7 @@
 
 def solution(num_list):
-    mul = 1 
-    for n in num_list:
-        mul *= n
-    return int(mul < sum(num_list) ** 2)
+    mul = 1
+    for i in num_list:
+        mul *= i
+    return int(mul < sum(num_list)**2)
+
