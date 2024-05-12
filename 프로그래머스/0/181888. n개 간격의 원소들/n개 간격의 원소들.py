0be794cb70a22e31d33b485179380a38787dc436
ROH SI EUN <151046260+sieunrroh@users.noreply.github.com>
2024-05-12 21:15:47 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.py
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -1,7 +1,9 @@
 def solution(num_list, n):
     answer = []
     
-    for i in range(0,len(num_list),n):
+    for i in range(0,len(num_list),n): # 리스트 추가이므로, append임 
            answer.append(num_list[i]) 
     return answer
 
+
+# 문자열 이어붙이기는 +=
